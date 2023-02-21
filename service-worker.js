a2dc772e0229832cb50b5acd8386c0b0820a6946
dumitru-stratulat <62725538+dumitru-stratulat@users.noreply.github.com>
2023-02-21 12:14:31 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,12 +6,41 @@ var cacheFiles = [
 "icon.png",
   "style.css"
 ];
-self.addEventListener("install", function(e) {
-console.log("[Service Worker] Install");
-e.waitUntil(
-caches.open(cacheName).then(function(cache) {
-console.log("[Service Worker] Caching files");
-return cache.addAll(cacheFiles);
-})
-);
+var cacheName = "courseStore-v1";
+
+var cacheFiles = [
+    "courseShop.html",
+    "vueInstance.js",
+    "icon.png",
+    "style.css"
+];
+self.addEventListener("install", function (e) {
+    console.log("[Service Worker] Install");
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log("[Service Worker] Caching files");
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
+self.addEventListener("fetch", function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (cachedFile) {
+            //download the file if it is not in the cache
+            if (cachedFile) {
+                console.log("[Service Worker] Resource fetched from the cache for: " + e.request.url);
+                return cachedFile;
+            } else {
+                return fetch(e.request).then(function (response) {
+                    return caches.open(cacheName).then(function (cache) {
+                        //add the new file to the cache
+                        cache.put(e.request, response.clone());
+                        console.log("[Service Worker] Resource fetched and saved in the cache for: " +
+                            e.request.url);
+                        return response;
+                    });
+                });
+            }
+        })
+    );
 });
